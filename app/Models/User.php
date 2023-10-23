27eb646beb028d7be59ece98335eaefc2b05ee5f
faizurraheman <faizurrheman75@gmail.com>
2023-10-23 10:59:37 +0530

    edit data

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,24 +10,22 @@ class User extends Model
     protected $table            = 'users';
     protected $primaryKey       = 'id';
     protected $useAutoIncrement = true;
+    protected $insertID         = 0;
     protected $returnType       = 'array';
     protected $useSoftDeletes   = false;
     protected $protectFields    = true;
-    protected $allowedFields    = [];
-
+    protected $allowedFields    = ['name', 'age'];
     // Dates
     protected $useTimestamps = false;
     protected $dateFormat    = 'datetime';
     protected $createdField  = 'created_at';
     protected $updatedField  = 'updated_at';
     protected $deletedField  = 'deleted_at';
-
     // Validation
     protected $validationRules      = [];
     protected $validationMessages   = [];
     protected $skipValidation       = false;
     protected $cleanValidationRules = true;
-
     // Callbacks
     protected $allowCallbacks = true;
     protected $beforeInsert   = [];
